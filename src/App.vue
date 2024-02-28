6c2931fbc81ebdf0e7cdebf57083efe59e5e0f9c
jkimdev <jkim.dev@icloud.com>
2024-02-28 13:53:15 +0900

    complete about view

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div class="sticky-top wrapper">
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <!-- <div class="wrapper"> -->
-    <!-- <HelloWorld msg="You did it!" /> -->
-    <nav class="navbar navbar-dark bg-dakr">
+ 
+    <nav class="navbar navbar-dark bg-dakr" style="background-color: #222529;">
       <a class="navbar-brand" href="#">
-        <img src="" width="30" height="30" class="d-inline-block align-top" alt="" />
-        JOBSHOPER
+        <img src="./assets/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="" />
+        JJOBSHOPER
       </a>
       <div class="links">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/">About</RouterLink>
+      <RouterLink to="/about">Projects</RouterLink>
     </div>
     </nav>
-    <!-- </div> -->
   </header>
 </div>
 
@@ -28,25 +23,15 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-/* .logo {
-  display: block;
-  margin: 0 auto 2rem;
-} */
-
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
 }
 
-nav a.router-link-exact-active {
+/* nav a.router-link-exact-active {
   color: white;
-}
+} */
 
 nav a.router-link-exact-active:hover {
   background-color: transparent;
@@ -84,31 +69,4 @@ nav a:first-of-type {
   padding-left: 4rem;
 }
 }
-
-/* @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-} */
 </style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,55 @@
 <script setup></script>
 
 <template>
-  <p>Hello World</p>
+  <div class="container pt-4">
+    <div class="about-title">
+      <h1 class="font-weight-bold">About</h1>
+      <p>
+        Educators are developing innovative technologies to provide more effective education to a
+        greater number of students.
+      </p>
+      <div class="border-top pb-4"></div>
+    </div>
+    <div class="about-content pb-4">
+      <img class="img-fluid rounded mx-auto d-block about-img" src="../assets/images/about.svg" />
+      <span>
+        <p class="about-text">
+          The domestic private education market is extraordinarily mature and massive, unlike
+          anywhere else in the world. Academy directors possess dual identities: as educators who
+          observe students daily and as entrepreneurs who study and ponder daily for survival.
+          Recognizing that private education has become indispensable domestically, they acknowledge
+          it as an industry, supplying technologies through academies to provide more effective
+          education at lower prices.
+        </p></span
+      >
+    </div>
+    <div class="border-top pb-4"></div>
+    <div class="contributors">
+      <h1 class="font-weight-bold">Contributors</h1>
+      <img class="img-fluid rounded" src="../assets/images/contributors.jpg" alt="" />
+    </div>
+  </div>
 </template>
+
+<style>
+.about-img {
+  width: 10rem;
+  height: 10rem;
+}
+
+.about-text {
+  display: inline-block;
+  padding-top: 1rem;
+}
+
+@media (min-width: 1024px) {
+  .about-content {
+    display: flex;
+    align-items: top;
+  }
+  .about-text {
+    padding-left: 1rem;
+    padding-top: 0;
+  }
+}
+</style>
