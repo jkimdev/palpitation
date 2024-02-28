19a827dfab75ec2784ed3768dbede9f2a29b5e21
jkimdev <jkim.dev@icloud.com>
2024-02-28 19:03:27 +0900

    projects with card view

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <div class="container pt-4">
+    <div class="card">
+      <slot class="project-image" name="image"> </slot>
+      <div class="card-body">
+        <h5 class="card-title">
+          <slot name="card-title"></slot>
+        </h5>
+        <p class="card-text">
+          <slot name="card-text"> </slot>
+        </p>
+        <a href="#" class="card-link">Learn more →</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+a, a:visited {
+    text-decoration: none;
+    color: white
+}
+</style>
+
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,49 @@
+<script setup>
+import ProjectCard from '@/components/ProjectCard.vue'
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+    <div class="col">
+      <ProjectCard>
+        <template #image>
+          <img src=".././assets/images/mining.jpg" class="card-img-top" alt="..." style="height: 200px;"/>
+        </template>
+        <template #card-title>세특학원 학쫑프로</template>
+        <template #card-text>
+          <ul>
+            <li>[2023.07] 학쫑프로 수시배치표 런칭</li>
+            <li>[2023.07] 학쫑프로 정식런칭</li>
+          </ul>
+        </template>
+      </ProjectCard>
+    </div>
+    <div class="col">
+      <ProjectCard>
+        <template #image>
+          <img src=".././assets/images/brain.jpg" class="card-img-top" alt="..." style="height: 200px;"/>
+        </template>
+        <template #card-title>맞춤형 비교과 B2C 솔루션 세특여왕</template>
+        <template #card-text>
+          <ul>
+            <li>[2023.07] 학쫑프로 수시배치표 런칭</li>
+            <li>[2023.07] 학쫑프로 정식런칭</li>
+          </ul>
+        </template>
+      </ProjectCard>
+    </div>
+    <div class="col">
+      <ProjectCard>
+        <template #image>
+          <img src=".././assets/images/test.jpg" class="card-img-top" alt="..." style="height: 200px;"/>
+        </template>
+        <template #card-title>AI 학습용 데이터 수집/가공 플랫폼 린에이아이(Lean AI)</template>
+        <template #card-text>
+          <ul>
+            <li>[2023.07] 학쫑프로 수시배치표 런칭</li>
+            <li>[2023.07] 학쫑프로 정식런칭</li>
+          </ul>
+        </template>
+      </ProjectCard>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
