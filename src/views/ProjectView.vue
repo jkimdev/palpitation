5630b8e2b4d1b0afd523a86e0809b20a505c61ff
jkimdev <jkim.dev@icloud.com>
2024-02-28 20:17:47 +0900

    change view name for project view

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -6,7 +6,7 @@ import ProjectCard from '@/components/ProjectCard.vue'
     <div class="col">
       <ProjectCard>
         <template #image>
-          <img src=".././assets/images/mining.jpg" class="card-img-top" alt="..." style="height: 200px;"/>
+          <img src=".././assets/images/mining.jpg" class="card-img-top" alt="..." style="height: 10rem;"/>
         </template>
         <template #card-title>세특학원 학쫑프로</template>
         <template #card-text>
@@ -20,7 +20,7 @@ import ProjectCard from '@/components/ProjectCard.vue'
     <div class="col">
       <ProjectCard>
         <template #image>
-          <img src=".././assets/images/brain.jpg" class="card-img-top" alt="..." style="height: 200px;"/>
+          <img src=".././assets/images/brain.jpg" class="card-img-top" alt="..." style="height: 10rem;"/>
         </template>
         <template #card-title>맞춤형 비교과 B2C 솔루션 세특여왕</template>
         <template #card-text>
@@ -34,7 +34,7 @@ import ProjectCard from '@/components/ProjectCard.vue'
     <div class="col">
       <ProjectCard>
         <template #image>
-          <img src=".././assets/images/test.jpg" class="card-img-top" alt="..." style="height: 200px;"/>
+          <img src=".././assets/images/test.jpg" class="card-img-top" alt="..." style="height: 10rem;"/>
         </template>
         <template #card-title>AI 학습용 데이터 수집/가공 플랫폼 린에이아이(Lean AI)</template>
         <template #card-text>
